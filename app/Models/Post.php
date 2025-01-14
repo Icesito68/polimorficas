960f17e61a138d170f1abf0494f3808cc24f3fbe
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-01-14 10:54:18 +0000

    Create the latest models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Post extends Model
 {
@@ -13,5 +15,15 @@ class Post extends Model
 
     public function comments(): MorphMany{
         return $this->morphMany(Comment::class, "commentable");
+    }
+
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
     }
 }
